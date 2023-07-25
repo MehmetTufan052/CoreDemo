a442ae30b22e285cc0819d7afb8fb5fab6d25794
MehmetTufan052 <tufaneser8@gmail.com>
2023-07-25 18:20:09 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,35 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-MO1FN9D;database=CoreBlogDb;integrated security=true");
+
+            //Context alanında server adını ve database ismini tanımladık OnConfiguring ile ve DbContextOptionsBuilder kullanarak core'da connection string oluşturmuş olduk
+        }
+
+        public DbSet<About> Abouts { get; set; }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Writer> Writers { get; set; }
+
+       
+    }
+}
